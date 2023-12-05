dbf8cbd443527181f13952be54cf010c89660d3e
Visibyte <nathanglass26@gmail.com>
2023-12-05 14:29:44 +0000

    delete home page test

--- src/app/page.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Home from "./page";
-
-it("Should render the heading", () => {
-  render(<Home />);
-  expect(screen.getByText("Testing 1 2 3")).toBeInTheDocument();
-});
